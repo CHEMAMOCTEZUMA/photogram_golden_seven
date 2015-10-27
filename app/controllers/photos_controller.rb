e6435ed95b0a8f818a681d12f4781633df24d829
CHEMAMOCTEZUMA <jmoctezu@chicagobooth.edu>
2015-10-26 19:32:19 -0500

    Done!

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,4 +8,45 @@ class PhotosController < ApplicationController
   	@photo = Photo.find_by({:id => params[:id]})
   end
 
+  def new_form
+
+  end
+
+  def create_row
+
+  	@new = Photo.new 
+  	@new.caption = params[:caption]
+  	@new.source = params[:source]
+  	@new.save
+
+  	redirect_to("http://localhost:3000/photos")
+
+  end
+
+  def destroy
+
+	@delete = Photo.find_by({:id => params[:id]})
+	@delete.destroy
+
+  	redirect_to("http://localhost:3000/photos")
+
+  end
+
+  def edit
+
+	@photo = Photo.find_by({:id => params["id"]})
+
+  end
+
+  def update
+
+	@photo = Photo.find_by({:id => params["id"]})
+  	@photo.caption = params[:caption]
+  	@photo.source = params[:source]
+  	@photo.save	
+
+  	redirect_to("http://localhost:3000/photos")
+
+  end
+
 end
